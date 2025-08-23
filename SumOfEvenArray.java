ddd6cb71d1050f4a71883e3aedaa85e090718709
Sindhu Ravuri <sindhuravuri666@gmail.com>
2025-08-23 10:01:32 +0530

    Arrays

---
+++ SumOfEvenArray.java
@@ -0,0 +1,13 @@
+public class SumOfEvenArray {
+    public static void main(String[] args) {
+        int arr[] = {1,2,3,4,5,6};
+        int sum = 0;
+        for(int i = 0;i < arr.length;i++){
+            if(i % 2 == 0){
+                sum += arr[i];
+            }
+            System.out.println("Sum of even numbers in array: " + sum);
+
+        }
+    }
+}
