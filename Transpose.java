eb321146dd7b3e9eb61dbfc33a6f99ea51284538
Sindhu Ravuri <sindhuravuri666@gmail.com>
2025-08-26 10:27:09 +0530

    DSA

---
+++ Transpose.java
@@ -0,0 +1,13 @@
+public class Transpose {
+    public static void main(String[] args) {
+        int[][] matrix = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+    
+    }
+}
