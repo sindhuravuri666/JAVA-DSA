1662cddaa55cddda41746a374a1037207215593a
Sindhu Ravuri <sindhuravuri666@gmail.com>
2025-10-08 12:25:35 +0530

    MatrixSymmetry.java

---
+++ MatrixSymmetry.java
@@ -0,0 +1,31 @@
+public class MatrixSymmetry {
+    public static void main(String[] args) {
+        int[][] matrix = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+
+        boolean isSymmetric = true;
+        int n = matrix.length;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] != matrix[j][i]) {
+                    isSymmetric = false;
+                    break;
+                }
+            }
+            if (!isSymmetric) {
+                break;
+            }
+        }
+
+        if (isSymmetric) {
+            System.out.println("The matrix is symmetric.");
+        } else {
+            System.out.println("The matrix is not symmetric.");
+        }
+    }
+    
+}
