d6c713d93f52807855bf9a399d0f680a993d800d
Sindhu Ravuri <sindhuravuri666@gmail.com>
2025-10-09 10:21:47 +0530

    LeftRotation.java

--- LeftRotation.java
+++ LeftRotation.java
@@ -3,9 +3,12 @@ public class LeftRotation {
     public static void leftRotate(int[] arr, int k) {
         int n = arr.length;
         k = k % n; 
+        // this reverses the first k elements if k = 2 and arr = {1,2,3,4,5} then arr becomes {2,1,3,4,5}
         reverse(arr, 0, k - 1);
+        // this reverses the elements from k to end if arr = {2,1,3,4,5} then arr becomes {2,1,5,4,3}
         
         reverse(arr, k, n - 1);
+        // this reverses the whole array if arr = {2,1,5,4,3} then arr becomes {3,4,5,1,2}
       
         reverse(arr, 0, n - 1);
     }
@@ -30,3 +33,18 @@ public class LeftRotation {
     }
     
 }
+
+
+
+
+
+
+// collections in java  means data structures which are used to store group of objects as a single unit.
+// examples of collections are ArrayList, LinkedList, HashSet, HashMap etc.
+// collections in java  means data structures which are used to store group of objects as a single unit.
+// interfaces in java are abstract types that allow the creation of methods that one or more classes will implement.
+// examples of interfaces are List, Set, Map etc.
+// set is a collection that contains no duplicate elements and at most one null element.set implements hashset, linkedhashset, treeset etc.
+// list is an ordered collection (also known as a sequence).list implements arraylist, linkedlist, vector etc.
+// map is an object that maps keys to values.map implements hashmap, linkedhashmap, treemap etc.
+// arraylist is a resizable array implementation of the list interface.arraylist allows duplicate elements and maintains insertion order.
